0e8e56800ddf0d278d318441cb2b71ca84d6eb2d
manikmahardikaa <127854755+manikmahardikaa@users.noreply.github.com>
2025-08-13 15:19:15 +0800

    feat: web-sehati-v1

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import "./globals.css";
 import GlobalProvider from "./components/global-provider";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+export const fetchCache = "force-no-store"; 
 
 export default function RootLayout({
   children,
